4f04b819cae1b8bb7aec6ab2efbeb45705afc9e9
Cho, Seoyun <whtjdbs3170@gmail.com>
2025-10-20 15:52:19 +0900

    four

--- src/k202417053/Main.java
+++ src/k202417053/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("조서윤");
 		System.out.println("안녕하세요");
-
+		World.run();
 	}
 
 }
---
+++ src/k202417053/World.java
@@ -0,0 +1,7 @@
+package k202417053;
+
+public class World {
+	public static void run() { 
+		System.out.println("Hello, World!"); 
+	}
+}
